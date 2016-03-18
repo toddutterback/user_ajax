28b9685d2116c80011fcacdd1728ca31adcee91f
Todd Utterback <todd.j.utterback@gmail.com>
2016-03-18 09:35:38 -0600

    Get, Put, Delete

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -25,4 +25,43 @@ $(document).ready(function() {
   });
  
 
+  var re = /\/welcome\/\d+/;
+  if (location.pathname.match(re)) {
+    var panel = $('#panel');
+    var id = panel.data('id');
+    $(document).on('click', '#remove', function() {
+      $.ajax({
+        url: baseUrl + '/' + id, 
+        type: 'DELETE',
+        dataType: 'JSON',
+        success: function() {
+          location.href = '/';
+        },
+        error: function(error) {
+          location.href = '/';
+        }
+      });
+    });
+    $.ajax({
+      url: baseUrl + '/' + id,
+      type: 'GET',
+      dataType: 'JSON',
+      success: function(data) {
+        var user = data.user
+        panel.children('#heading').html(user.first_name);
+        var list = $('#user');
+        var first_name = '<li>First Name: ' + user.first_name + '</li>';
+        var last_name =  '<li>Last Name: ' + user.last_name + '</li>';
+        var phone_number =  '<li>Phone Number: ' + user.phone_number + '</li>';
+        var remove = '<li><button class="btn btn-danger" id="remove">Delete</button></li>';
+        list.append(first_name);
+        list.append(last_name);
+        list.append(phone_number);
+        list.append(remove);
+      }
+    })
+  };   
+
+
+
 });
